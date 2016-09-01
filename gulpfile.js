6cdc95c4d54dd6bd025629dca25ea7cca95b7744
pspgbhu <hucong929@qq.com>
2016-09-01 20:56:53 +0800

    pspgbhu

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
+var gulpUglify = require('gulp-uglify');
+var gulpImg = require('gulp-imagemin');
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  console.log('hi~hi~hi~~~');
-});
+function errlog(err) {
+  console.error(err);
+  this.emit('end');
+}
 
-gulp.task('other', function () {
-    console.log('Hello Gulp Other Task');
-});
+
+gulp.task('default', ['script', 'watch']);
+
+
+gulp.task('watch', function () {
+  gulp.watch('public/javascripts/*.js', ['script'])
+})
+
+
+gulp.task('script', function () {
+  gulp.src('public/javascripts/*.js')
+      .on('error', errlog)   // 使用錯誤事件處理例外
+      .pipe(gulpUglify())
+      .pipe(gulp.dest('public/jsmin'))
+})
+
+gulp.task('img', function () {
+  
+})
